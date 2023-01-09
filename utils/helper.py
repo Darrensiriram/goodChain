bb34f984f4e595575b066b9aa8d7074789ba10f9
Darren Siriram <ds.siriram@gmail.com>
2023-01-09 16:09:40 +0100

    added new function to retrieve all the data of the chain - and created an function to retrieve username by pubc key

--- utils/helper.py
+++ utils/helper.py
@@ -1,12 +1,11 @@
 import pickle
-
+import sqlite3
+connection = sqlite3.Connection('database_actions/goodchain.db')
 def pluckStr(result: list, key):
     if key in result:
         return key
     else:
         return None
-
-
 
 def get_all_transaction_in_the_pool():
     allTx = []
@@ -17,3 +16,21 @@ def get_all_transaction_in_the_pool():
         except EOFError:
             pass
     return allTx
+
+def get_user_name_by_pub_key(con=connection, pbcKey=''):
+    cur = con.cursor()
+    result = cur.execute('SELECT username FROM users WHERE public_key = ?', (pbcKey,)).fetchone()
+    if result is None:
+        return None
+    return result[0]
+
+
+def get_all_tx_in_the_chain():
+    allTx = []
+    with open('data/block.dat', "rb+") as f:
+        try:
+            while True:
+                allTx.append(pickle.load(f))
+        except EOFError:
+            pass
+    return allTx[0]
