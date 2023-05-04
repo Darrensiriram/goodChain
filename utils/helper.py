5e674f03c434b64160ee151c53a91a4e92a45db8
Darren Siriram <ds.siriram@gmail.com>
2023-05-04 16:59:06 +0200

    removed shit code

--- utils/helper.py
+++ utils/helper.py
@@ -2,7 +2,6 @@ import pickle
 import sqlite3
 import hashlib
 import os
-#connection = sqlite3.Connection('database_actions/goodchain.db')
 def pluckStr(result: list, key):
     if key in result:
         return key
@@ -40,7 +39,6 @@ def get_all_tx_in_the_chain():
     else:
         return allTx[0]
 
-# fix that is also works on windowss
 def create_hash(file_path):
     sha256 = hashlib.sha256()
     with open(file_path, "rb") as f:
@@ -104,34 +102,3 @@ def validateBlock():
             return True
         else:
             return False
-
-
-# def broadcast(msg, localIP, port):
-#     try:
-#         s.getpeername()
-#     except socket.error:
-#         s.connect((localIP, port))
-#     print(f'Broadcasting message all over the network: {msg} ')
-#     s.sendall(msg.encode("UTF-8"))
-
-# def update_file(current_file, new_file):
-#     with open(new_file, 'rb') as f:
-#         new_data = f.read()
-#     with open(current_file, 'wb') as f:
-#         f.write(new_data)
-#     print(f'File {current_file} has been updated with data from {new_file}.')
-#
-#
-# def broadcast_file(filename, localIP, port):
-#     try:
-#         s.getpeername()
-#         print("Socket is already connected")
-#     except socket.error:
-#         s.connect((localIP, port))
-#
-#     with open(filename, 'rb') as f:
-#         filedata = f.read()
-#     pickled_data = pickle.dumps(filedata)
-#     print(f"File broadcasted all over the network: {filename}")
-#     s.sendall(pickled_data)
-#     print("File sent successfully")
